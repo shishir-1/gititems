fdbda27aed3b94f004bc940550f63e98c47c212e
shishir <shishirkumar98@gmail.com>
2021-09-29 23:58:49 +0530

    updated

--- scripts/scripts1.sh
+++ scripts/scripts1.sh
@@ -6,3 +6,13 @@
 #Input: None
 #Output:None
 ########################################################
+
+# clear te screen
+clear
+#give a msg opn screen
+echo "!!!! Welcome to the world of shell scripting!!!!"
+#Sleep for 10 sec
+sleep 10
+#Print the end msg
+echo "------- End--------"
+
